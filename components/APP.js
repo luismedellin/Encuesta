5c2a9b226819ba0a8fb4fd1f11e67312aa0b6df1
luismedellin1989 <luiseduardo1218@gmail.com>
2016-01-06 17:06:35 -0500

    02.01 Sockets test

--- components/APP.js
+++ components/APP.js
@@ -1,6 +1,17 @@
 var React 	= require('react');
+var io = 	require('socket.io-client');
 
 var APP = React.createClass({
+
+	componentWillMount(){
+		this.socket = io('http://localhost:3000');
+		this.socket.on('connect', this.connect);
+	},
+
+	connect(){
+		alert('Connect: '+this.socket.id);
+	},
+
 	render() {
 		return (<h1>Hola mundo</h1>);
 	}
